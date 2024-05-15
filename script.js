4e3fcc59ac78aed92fc72f4de20f65f56e021b96
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-05-16 00:59:39 +0500

    in progress

---
+++ script.js
@@ -0,0 +1,15 @@
+var img2 = document.getElementById('img2')
+img2.addEventListener('mousemove',function() {
+    img2.src = ''
+})
+img2.addEventListener('mouseover', function(){
+    img1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2JC1dCzToWnciTiCXlUo_jCaeru88zRPn-3HkmJx2LA&s'
+})
+var img3 = document.getElementById('img3')
+img3.addEventListener('mousemove',function(){
+    img1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2JC1dCzToWnciTiCXlUo_jCaeru88zRPn-3HkmJx2LA&s'
+})
+var img4 = document.getElementById('img4')
+img4.addEventListener('mousemove',function(){
+    img1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2JC1dCzToWnciTiCXlUo_jCaeru88zRPn-3HkmJx2LA&s'
+})
