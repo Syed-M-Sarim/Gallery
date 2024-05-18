469b4c225a04c234067897e8135c0e48c18858e1
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-05-18 22:56:10 +0500

    changes

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 var img2 = document.getElementById('img2')
 img2.addEventListener('mouseover', function() {
+    img2.src='https://cdn.pixabay.com/photo/2018/10/05/11/58/anime-3725750_640.png'
 })
-
-img2.src='https://cdn.pixabay.com/photo/2018/10/05/11/58/anime-3725750_640.png'
 img2.addEventListener('mouseout', function() {
 img1.src='https://cdn.pixabay.com/photo/2023/04/09/18/54/misam-7912065_640.png'
 })
 
 var img3 = document.getElementById('img3')
 img3.addEventListener('mouseover',function() {
+    img3.src='https://cdn.pixabay.com/photo/2018/09/28/09/14/anime-3708780_640.png'
 })
-
-img3.src='https://cdn.pixabay.com/photo/2018/09/28/09/14/anime-3708780_640.png'
 img3.addEventListener("mouseout",function() {
 img1.src='https://cdn.pixabay.com/photo/2023/04/09/18/54/misam-7912065_640.png'
 })
 
 var img4 = document.getElementById('img4')
 img4.addEventListener('mouseover',function() {
+    img4.src='https://cdn.pixabay.com/photo/2018/10/05/11/58/anime-3725749_640.png'
 })
-
-img4.src='https://cdn.pixabay.com/photo/2018/10/05/11/58/anime-3725749_640.png'
 img4.addEventListener('mouseout',function() {
 })
-
 img1.src='https://cdn.pixabay.com/photo/2023/04/09/18/54/misam-7912065_640.png'
 var img5 = document.getElementById('img5')
 img5.addEventListener('mouseover',function() {
+    img5.src='https://cdn.pixabay.com/photo/2018/09/28/09/13/anime-3708774_640.png'
 })
-
-img5.src='https://cdn.pixabay.com/photo/2018/09/28/09/13/anime-3708774_640.png'
 img5.addEventListener('mouseout',function() {
 })
-
 img1.src='https://cdn.pixabay.com/photo/2023/04/09/18/54/misam-7912065_640.png'
